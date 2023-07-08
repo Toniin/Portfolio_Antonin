caf73a603c1ec1770321dc79c40a0bd0e2cc518c
Toniin <antonin.chaudiere@hotmail.fr>
2023-07-08 13:44:48 +0200

    Add map of Italy in footer, add quote in footer, add some experiences

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -150,8 +150,28 @@ const About: FunctionComponent = () => {
         />
       </svg>
       <div className="about__content w-full h-screen flex flex-col items-center">
-        <h2 className="text-xl">À propos</h2>
-        <p className="py-10">Mon expérience</p>
+        <h2 className="text-xl">Mes expériences</h2>
+        <p className="tooltip my-16 p-5 border-solid rounded bg-sky-900 text-white">
+          <span className="font-bold">
+            Formation développeur web (front-end, react)
+          </span>
+          <br />
+          OpenClassrooms
+          <br />
+          déc. 2022 - août.2023 · 9 mois
+          <br />À distance
+        </p>
+        <p className="tooltip my-4 p-5 border-solid rounded bg-sky-900 text-white">
+          <span className="font-bold">
+            Formation développeur web (front-end, react)
+          </span>
+          <br />
+          Crealead
+          <br />
+          oct. 2019 - nov.2019 · 2 mois
+          <br />
+          Région de Montpellier, France
+        </p>
       </div>
     </section>
   );
