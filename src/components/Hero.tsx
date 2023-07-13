3a31d290a18cb413a91a92f2fa4ff9ec668d8381
Toniin <antonin.chaudiere@hotmail.fr>
2023-07-13 10:13:58 +0200

    Fix rectangle hyperspace into circle

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,18 +7,18 @@ const Hero: FunctionComponent = () => {
   const { theme } = useTheme();
 
   return (
-    <section className="banner flex items-center h-screen">
+    <section className="hero flex items-center h-screen">
       {theme === "dark" ? (
-        <div className="banner__stars">
-          <div className="banner__stars__little"></div>
-          <div className="banner__stars__medium"></div>
-          <div className="banner__stars__big"></div>
+        <div className="hero__stars">
+          <div className="hero__stars__little"></div>
+          <div className="hero__stars__medium"></div>
+          <div className="hero__stars__big"></div>
         </div>
       ) : (
         <></>
       )}
 
-      <div className="banner__content flex flex-col items-center gap-y-5">
+      <div className="hero__content flex flex-col items-center gap-y-5">
         <h1 className="text-5xl tracking-wider">ANTONIN</h1>
         <p className="text-xl mb-5 text-center">
           Développeur web front-end. Autodidacte.
@@ -36,7 +36,7 @@ const Hero: FunctionComponent = () => {
       </div>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="banner__timeline"
+        className="hero__timeline"
         width="100%"
         height="100%"
         viewBox="0 0 100 250"
@@ -49,82 +49,82 @@ const Hero: FunctionComponent = () => {
         />
         {theme === "dark" ? (
           <>
-            <g className="banner__timeline__hyperspace banner__timeline__hyperspace__right">
-              <path
-                className="banner__timeline__hyperspace__right__1"
-                d="M0,211 1,210.5"
-                fill="transparent"
-                strokeWidth="0.5"
+            <g className="hero__timeline__hyperspace hero__timeline__hyperspace__right">
+              <circle
+                className="hero__timeline__hyperspace__right__1"
+                cx="0"
+                cy="211"
+                r="0.5"
               />
-              <path
-                className="banner__timeline__hyperspace__right__2"
-                d="M0,210 1,209.5"
-                fill="transparent"
-                strokeWidth="0.9"
+              <circle
+                className="hero__timeline__hyperspace__right__2"
+                cx="0"
+                cy="210"
+                r="0.4"
               />
-              <path
-                className="banner__timeline__hyperspace__right__3"
-                d="M0,208 1,207.5"
-                fill="transparent"
-                strokeWidth="1"
+              <circle
+                className="hero__timeline__hyperspace__right__3"
+                cx="0"
+                cy="208"
+                r="0.7"
               />
-              <path
-                className="banner__timeline__hyperspace__right__4"
-                d="M0,214 1,213.5"
-                fill="transparent"
-                strokeWidth="0.7"
+              <circle
+                className="hero__timeline__hyperspace__right__4"
+                cx="0"
+                cy="214"
+                r="0.6"
               />
-              <path
-                className="banner__timeline__hyperspace__right__5"
-                d="M0,205 1,204.5"
-                fill="transparent"
-                strokeWidth="0.6"
+              <circle
+                className="hero__timeline__hyperspace__right__5"
+                cx="0"
+                cy="205"
+                r="0.9"
               />
             </g>
 
-            <g className="banner__timeline__hyperspace banner__timeline__hyperspace__left">
-              <path
-                className="banner__timeline__hyperspace__left__1"
-                d="M0,211 -1,210.5"
-                fill="transparent"
-                strokeWidth="0.5"
+            <g className="hero__timeline__hyperspace hero__timeline__hyperspace__left">
+              <circle
+                className="hero__timeline__hyperspace__left__1"
+                cx="0"
+                cy="211"
+                r="0.5"
               />
-              <path
-                className="banner__timeline__hyperspace__left__2"
-                d="M0,210 -1,209.5"
-                fill="transparent"
-                strokeWidth="0.9"
+              <circle
+                className="hero__timeline__hyperspace__left__2"
+                cx="0"
+                cy="210"
+                r="0.4"
               />
-              <path
-                className="banner__timeline__hyperspace__left__3"
-                d="M0,208 -1,207.5"
-                fill="transparent"
-                strokeWidth="1"
+              <circle
+                className="hero__timeline__hyperspace__left__3"
+                cx="0"
+                cy="208"
+                r="0.7"
               />
-              <path
-                className="banner__timeline__hyperspace__left__4"
-                d="M0,214 -1,213.5"
-                fill="transparent"
-                strokeWidth="0.7"
+              <circle
+                className="hero__timeline__hyperspace__left__4"
+                cx="0"
+                cy="214"
+                r="0.6"
               />
-              <path
-                className="banner__timeline__hyperspace__left__5"
-                d="M0,205 -1,204.5"
-                fill="transparent"
-                strokeWidth="0.6"
+              <circle
+                className="hero__timeline__hyperspace__left__5"
+                cx="0"
+                cy="205"
+                r="0.9"
               />
             </g>
           </>
         ) : (
           <>
-            <g className="banner__timeline__leaf">
+            <g className="hero__timeline__leaf">
               <path
                 d="M0,220 C 0,215 5,215 7,207"
                 fill="transparent"
                 strokeWidth="1"
               />
               <path
-                className="banner__timeline__leaf--fill"
+                className="hero__timeline__leaf--fill"
                 d="M5,212 C 5,210 2,205 10,200 C 10,200 15,205 5,212"
                 fill="transparent"
                 strokeWidth="1"
@@ -137,14 +137,14 @@ const Hero: FunctionComponent = () => {
                 strokeWidth="1"
               />
             </g>
-            <g className="banner__timeline__leaf">
+            <g className="hero__timeline__leaf">
               <path
                 d="M0,215 C 0,210 -5,210 -5,204"
                 fill="transparent"
                 strokeWidth="1"
               />
               <path
-                className="banner__timeline__leaf--fill"
+                className="hero__timeline__leaf--fill"
                 d="M-5.5,198 C -5,200 0,205 -3,210 C -7.5,211 -10,200 -5,198"
                 fill="transparent"
                 strokeWidth="1"
