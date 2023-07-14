5980cb791bb9ca320fdc72fc3e61b4ec5bc322a2
Toniin <antonin.chaudiere@hotmail.fr>
2023-07-14 19:07:25 +0200

    Add effect on card projects + responsive projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,86 +1,65 @@
-import { FunctionComponent, useRef, useLayoutEffect } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { FunctionComponent, useRef, useEffect } from "react";
 import Timeline from "./Timeline";
 import projectsData from "../mockdata/projectsData.json";
 
 const projects: Array<ProjectData> = [...projectsData];
 
-gsap.registerPlugin(ScrollTrigger);
-
 const Projects: FunctionComponent = () => {
-  const projectsSection: React.RefObject<HTMLElement> =
-    useRef() as React.MutableRefObject<HTMLDivElement>;
+  useEffect(() => {
+    const projectsCard = document.querySelectorAll(".projects__content__card");
 
-  useLayoutEffect(() => {
-    const ctx = gsap.context((self) => {
-      if (projectsSection.current != null && self.selector) {
-        const projects = self.selector(".wrapper__project");
-        const containerWidth: number = projectsSection.current.clientWidth;
+    projectsCard.forEach((project) => {
+      project.addEventListener("mousemove", (event) => {
+        const CardRect = project.getBoundingClientRect();
 
-        projects.forEach((project: HTMLElement, index: number) => {
-          const projectCardsWidth: number = project.clientWidth;
-          const remainderWidth: number =
-            (containerWidth - projectCardsWidth * projects.length) / 2;
+        const x = event.clientX - CardRect.x;
+        const y = event.clientY - CardRect.y;
 
-          gsap
-            .timeline()
-            .to(project, {
-              scrollTrigger: {
-                trigger: project,
-                start: "bottom bottom",
-                end: "top 20%",
-                scrub: 1,
-              },
-              top: "50%",
-              x: "50%",
-              scale: 1,
-              rotate: 0,
-              left: projectCardsWidth * index + remainderWidth * index,
-              ease: "power3.inOut",
-            })
-            .to(project.children[1], {
-              scrollTrigger: {
-                trigger: project.children[1],
-                scrub: true,
-              },
-              top: "0",
-              scale: 1,
-            });
-        });
-      }
-    }, projectsSection); // <- Scope!
-    return () => ctx.revert(); // <- Cleanup!
+        const midCardWidth = CardRect.width / 2;
+        const midCardHeight = CardRect.height / 2;
+
+        const angleY = -(x - midCardWidth) / 10;
+        const angleX = (y - midCardHeight) / 10;
+
+        project.children[0].style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(2)`;
+      });
+
+      project.addEventListener("mouseleave", () => {
+        project.children[0].style.transform = `rotateX(0) rotateY(0)`;
+      });
+    });
   }, []);
 
   return (
-    <section ref={projectsSection} className="projects">
+    <section className="projects min-h-screen">
       <Timeline />
-      <div className="projects__content w-full h-screen flex flex-col items-center">
+      <div className="projects__content w-full h-full flex flex-col items-center">
         <h2 className="text-2xl">PROJETS</h2>
-        {projects.map((project, index) => (
-          <div
-            key={`${project.name} - ${index}}`}
-            className={`wrapper__project wrapper__project__${index + 1}`}
-          >
-            <img
-              src={project.imageSrc}
-              alt={project.imageAlt}
-              className="h-full w-full object-cover object-center"
-            />
-            <div className="wrapper__project__detail">
-              <h3 className="mt-6 text-sm text-gray-500">
-                <a href={project.href} target="_blank">
-                  <span className="absolute inset-0" />
-                  {project.name}
-                </a>
-              </h3>
-              <p className="text-base font-semibold text-gray-900">
-                {project.description}
-              </p>
-            </div>
-          </div>
-        ))}
+
+        <div className="flex flex-col items-center mt-24 gap-10 lg:flex-row lg:gap-6 lg:mt-48 mx-3">
+          {projects.map((project, index) => (
+            <a
+              href={project.href}
+              target="_blank"
+              key={`${project.name} - ${index}}`}
+              className="projects__content__card w-9/12 lg:w-1/3 neomorphism--light dark:neomorphism--dark"
+            >
+              <div className="projects__content__card__content w-full h-full overflow-hidden rounded-md">
+                <img
+                  src={project.imageSrc}
+                  alt={project.imageAlt}
+                  className="w-full object-cover"
+                />
+                <div className="p-2 bg-light dark:primary-color">
+                  <h3 className="text-lg text-center">{project.name}</h3>
+                  <p className="text-base font-semibold">
+                    {project.description}
+                  </p>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
       </div>
     </section>
   );
