46f32528a8c5b3f693af03fa73941e0e39ab333e
Toniin <antonin.chaudiere@hotmail.fr>
2023-07-24 13:32:31 +0200

    Add max width on projects cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent, useEffect } from "react";
 import { FaFileCode } from "react-icons/fa6";
 import Timeline from "./Timeline";
-import Timeline3Branchs from './Timeline3Branchs';
+import Timeline3Branchs from "./Timeline3Branchs";
 import projectsData from "../mockdata/projectsData.json";
 
 const projects: Array<ProjectData> = [...projectsData];
@@ -18,10 +18,10 @@ const Projects: FunctionComponent = () => {
         const CardRect = projectLink.getBoundingClientRect();
         const x = mouseEvent.clientX - CardRect.x;
         const y = mouseEvent.clientY - CardRect.y;
-        const midCardWidth = CardRect.width / 2;
-        const midCardHeight = CardRect.height / 2;
-        const angleY = -(x - midCardWidth) / 10;
-        const angleX = (y - midCardHeight) / 10;
+        const middleCardWidth = CardRect.width / 2;
+        const middleCardHeight = CardRect.height / 2;
+        const angleY = -(x - middleCardWidth) / 10;
+        const angleX = (y - middleCardHeight) / 10;
         projectCard.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(2)`;
       });
       projectLink.addEventListener("mouseleave", () => {
@@ -41,19 +41,13 @@ const Projects: FunctionComponent = () => {
           <FaFileCode size="1.2em" />
           PROJETS
         </h2>
-
-        {/* 
-          À FAIRE
-          Si écran horizontal => flex-row (règle le soucis de déformation sur petit écran horizontal)
-          Sinon => flex-col items-center mt-24 gap-10 lg:flex-row lg:gap-6 lg:mt-48 mx-3
-        */}
         <div className="flex flex-col items-center mt-20 gap-10 lg:flex-row lg:gap-6 lg:mt-32 mx-3">
           {projects.map((project, index) => (
             <a
               href={project.href}
               target="_blank"
               key={`${project.name} - ${index}}`}
-              className="projects__content__card w-9/12 lg:w-1/3 neomorphism--light dark:neomorphism--dark"
+              className="projects__content__card max-w-sm w-9/12 lg:w-1/3 neomorphism--light dark:neomorphism--dark"
             >
               <div className="projects__content__card__content w-full h-full overflow-hidden rounded-md">
                 <img
@@ -61,11 +55,7 @@ const Projects: FunctionComponent = () => {
                   alt={project.imageAlt}
                   className="w-full object-cover"
                 />
-                {/* 
-                  À FAIRE
-                  Mettre un box shadow au niveau texte (effet ombre) 
-                */}
-                <div className="p-2 bg-light dark:primary-color">
+                <div className="p-2 bg-light dark:primary-color shadow-inner">
                   <h3 className="text-lg text-center">{project.name}</h3>
                   <p className="text-base font-semibold">
                     {project.description}
